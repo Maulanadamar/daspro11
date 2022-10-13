417d2eeac90f4fb164c85bd28f8a5a2302b7881b
Maulanadamar <113022357+Maulanadamar@users.noreply.github.com>
2022-10-13 07:32:32 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   /*cout << " Bilangan genap 1- 100 "<< endl;
+   cout << endl;
+   for ( int genap =0 ; genap <= 100 ;genap++){
+    if(genap % 2 ==0)
+        cout <<  genap << endl;
+   }
+   cout << " Bilangan ganjil 1- 100"<<endl;
+   cout << endl;
+   for ( int ganjil =0 ; ganjil <= 100 ;ganjil++){
+    if(ganjil % 2 ==1)
+        cout << ganjil << endl;
+   }
+   cout << endl;
+   cout << " Bilangan Kelipatan 6 dari 1-100 "<< endl;
+   cout << endl;
+   for (int kelipatan6 = 6; kelipatan6 <= 100 ; kelipatan6++){
+    if ( kelipatan6 % 6 == 0)
+        cout << kelipatan6 << endl;
+   }*/
+   for(int i = 0 ; i < 2 ; i++) {
+    cout << i << endl;
+   for(int j =  0 ; j < 3 ; j++) {
+    cout << j  << endl;
+   }
+   }
+  return 0;
+}
